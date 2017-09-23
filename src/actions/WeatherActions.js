47a7d12f6851fa0696e60f05e09c537c4bc0be6a
Wouter Van Vlaenderen <wouter.vanvlaenderen@aca-it.be>
2017-09-23 08:46:40 +0200

    Refactoring getWeather action

--- src/actions/WeatherActions.js
+++ src/actions/WeatherActions.js
@@ -4,16 +4,16 @@ import axios from 'axios'
 export function getWeather () {
   let endpoint = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=2a345681ddcde393253af927097f5747'
 
-  return function (dispatch) {
+  return (dispatch) => {
     return axios.get(endpoint)
     .then((response) => {
-      return dispatch({
+      dispatch({
         type: GET_WEATHER_DONE,
         payload: response.data
       })
     })
     .catch((error) => {
-      return dispatch({
+      dispatch({
         type: GET_WEATHER_ERROR,
         payload: error.response.data,
         statuscode: error.response.status
